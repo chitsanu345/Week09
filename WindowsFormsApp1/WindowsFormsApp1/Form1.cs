abb3c011e1fde4990a944e5f3f70a805585784fd
chitsanu345 <chitsanu345@gmail.com>
2017-10-22 22:49:38 +0700

    W09P08

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -25,12 +25,11 @@ namespace WindowsFormsApp1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            System.Drawing.Drawing2D.HatchBrush mybrush = new System.Drawing.Drawing2D.HatchBrush(System.Drawing.Drawing2D.HatchStyle.DarkVertical,
-            Color.White, Color.Violet);
-            Pen mypen = new Pen(mybrush, 5);
-            g.DrawEllipse(mypen, 10, 10, 200, 200);
-            mypen.Dispose();
-            mybrush.Dispose();
+            Pen blackPen = new Pen(Color.Black, 3);
+            Point point1 = new Point(100, 100);
+            Point point2 = new Point(200, 200);
+            e.Graphics.DrawLine(blackPen, point1, point2);
+            blackPen.Dispose();
 
 
 

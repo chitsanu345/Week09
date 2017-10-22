17b13ce1a014342c80c122927bd4cfc0eeda849e
chitsanu345 <chitsanu345@gmail.com>
2017-10-22 22:54:12 +0700

    W09P10

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -25,12 +25,29 @@ namespace WindowsFormsApp1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Pen pen = new Pen(Color.Green, 2);
-            pen.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
-            e.Graphics.DrawLine(pen, 10, 80, 220, 80);
-            pen = new Pen(Brushes.DeepSkyBlue, 4);
-            e.Graphics.DrawLine(pen, 10, 120, 220, 120);
-            pen.Dispose();
+            Pen[] objPen = new Pen[11];
+            for (int i = 0; i < 11; i++)
+            {
+                objPen[i] = new Pen(Color.Blue, 9);
+            }
+            objPen[0].EndCap = System.Drawing.Drawing2D.LineCap.AnchorMask;
+            objPen[1].EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            objPen[2].EndCap = System.Drawing.Drawing2D.LineCap.Custom;
+            objPen[3].EndCap = System.Drawing.Drawing2D.LineCap.DiamondAnchor;
+            objPen[4].EndCap = System.Drawing.Drawing2D.LineCap.Flat;
+            objPen[5].EndCap = System.Drawing.Drawing2D.LineCap.NoAnchor;
+            objPen[6].EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            objPen[7].EndCap = System.Drawing.Drawing2D.LineCap.RoundAnchor;
+            objPen[8].EndCap = System.Drawing.Drawing2D.LineCap.Square;
+            objPen[9].EndCap = System.Drawing.Drawing2D.LineCap.SquareAnchor;
+            objPen[10].EndCap = System.Drawing.Drawing2D.LineCap.Triangle;
+
+            for (int i = 0; i < 11; i++)
+            {
+
+                g.DrawLine(objPen[i], 10, 10 + 20 * i, 200, 10 + 20 * i);
+                objPen[i].Dispose();
+            }
 
 
 

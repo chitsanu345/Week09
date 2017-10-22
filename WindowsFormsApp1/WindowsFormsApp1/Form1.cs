d5561c6deaeb15dad89d79dd4e74dbcd942dd234
chitsanu345 <chitsanu345@gmail.com>
2017-10-22 22:55:53 +0700

    W09P11

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -25,29 +25,13 @@ namespace WindowsFormsApp1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Pen[] objPen = new Pen[11];
-            for (int i = 0; i < 11; i++)
-            {
-                objPen[i] = new Pen(Color.Blue, 9);
-            }
-            objPen[0].EndCap = System.Drawing.Drawing2D.LineCap.AnchorMask;
-            objPen[1].EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
-            objPen[2].EndCap = System.Drawing.Drawing2D.LineCap.Custom;
-            objPen[3].EndCap = System.Drawing.Drawing2D.LineCap.DiamondAnchor;
-            objPen[4].EndCap = System.Drawing.Drawing2D.LineCap.Flat;
-            objPen[5].EndCap = System.Drawing.Drawing2D.LineCap.NoAnchor;
-            objPen[6].EndCap = System.Drawing.Drawing2D.LineCap.Round;
-            objPen[7].EndCap = System.Drawing.Drawing2D.LineCap.RoundAnchor;
-            objPen[8].EndCap = System.Drawing.Drawing2D.LineCap.Square;
-            objPen[9].EndCap = System.Drawing.Drawing2D.LineCap.SquareAnchor;
-            objPen[10].EndCap = System.Drawing.Drawing2D.LineCap.Triangle;
+            Pen Pen = new Pen(Color.Green, 2);
+            Point[] pt = { new Point(20,200),new Point(50,20),
+                           new Point(100,100), new Point(150,230),
+                           new Point(200,200)};
+            g.DrawCurve(Pen, pt);
+            g.Dispose();
 
-            for (int i = 0; i < 11; i++)
-            {
-
-                g.DrawLine(objPen[i], 10, 10 + 20 * i, 200, 10 + 20 * i);
-                objPen[i].Dispose();
-            }
 
 
 

ac44a6a1fc6a0a3eedbb4bd4ebd1dec94d73845d
chitsanu345 <chitsanu345@gmail.com>
2017-10-22 23:47:45 +0700

    W09P22

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -22,19 +22,35 @@ namespace WindowsFormsApp1
 
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-            Graphics g = e.Graphics;
-            Image image = Image.FromFile("d:\\Mountain.PNG");
-            //ใส่รูปโดยใช้ไฟล์ png เพื้อใช้เป็น Texture
-            TextureBrush brush = new TextureBrush(image);
-            Rectangle rect = new Rectangle(10, 10, 180, 150);
-            g.FillEllipse(brush, rect);
-            g.Dispose();
+       
+          private void Form1_Paint(object sender, PaintEventArgs e)
+            {
+
+                Graphics g = e.Graphics;
+                g.Dispose();
+            }
+
+
+            private void panel1_Paint_1(object sender, PaintEventArgs e)
+            {
+                System.Drawing.Drawing2D.LinearGradientBrush pnlGdt = new System.Drawing.Drawing2D.LinearGradientBrush(panel1.ClientRectangle,
+                Color.Yellow, Color.Navy, 0f, true);
+                e.Graphics.FillRectangle(pnlGdt, panel1.ClientRectangle);
+                pnlGdt.Dispose();
+            }
+
+            private void panel2_Paint(object sender, PaintEventArgs e)
+            {
+                System.Drawing.Drawing2D.LinearGradientBrush pnlGdt = new System.Drawing.Drawing2D.LinearGradientBrush(panel2.ClientRectangle,
+                Color.Yellow, Color.Navy, 90f, true);
+                e.Graphics.FillRectangle(pnlGdt, panel2.ClientRectangle);
+                pnlGdt.Dispose();
+            }
 
 
 
 
-        }
+
+        
     }
 }
